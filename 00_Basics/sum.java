533c27ebd4271ee494f7cc52b7d115fd2901818a
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-08 22:47:23 +0530

    sum of 2 numbers

---
+++ 00_Basics/sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+
+        int sum = a+b;
+
+        System.out.println("The answer is : "+sum);
+    }
+}
