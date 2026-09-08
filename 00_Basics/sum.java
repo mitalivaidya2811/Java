bc37856c4b594603dea7bd39e207660dd311c5f9
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-08 23:28:19 +0530

    sum

--- 00_Basics/sum.java
+++ 00_Basics/sum.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class sum {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number : ");
+        int b = sc.nextInt();
 
         int sum = a+b;
 
         System.out.println("The answer is : "+sum);
+
+        sc.close();
     }
 }
