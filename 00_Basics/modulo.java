9c6b3847962fbb2cf3bd18a53bac2b0f04065317
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-08 23:04:32 +0530

    Modulo

--- 00_Basics/modulo.java
+++ 00_Basics/modulo.java
@@ -1,11 +1,11 @@
 public class modulo {
     public static void main(String[] args) {
         int a = 5;
-        
+
         if(a % 10 == 0){
-            System.out.println("Its even number");
+            System.out.println("Its even number");  //for even number
         }else{
-            System.out.println("Its odd number");
+            System.out.println("Its odd number");   //for odd number
         }
     }
 }
