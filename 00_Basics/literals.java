2276b9c354ebe2509437176e48d95c3df545e2c1
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-09 17:11:31 +0530

    literals

---
+++ 00_Basics/literals.java
@@ -0,0 +1,22 @@
+public class literals {
+    public static void main(String[] args) {
+        byte age=34;
+        char ch='A';
+        float f1=5.6f;
+        float f2=6.3f;
+        float f3=7.5f;
+        double d1=4.66D;
+        double d2=5.77D;
+        double d3=3.44;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f3);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+
+    }
+    
+}
