74d42863887c30a89fc464b44cfc4626060a06d2
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-09 15:46:10 +0530

    sum3

---
+++ 00_Basics/sum3.java
@@ -0,0 +1,9 @@
+public class sum3 {
+    public static void main(String[] args) {
+        int a=4;
+        int b=5;
+        int c=a+b;
+        System.out.println("the sum of these number is :"+c);
+    }
+    
+}
