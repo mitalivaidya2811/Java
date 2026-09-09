ba50c8f6b609d4cf8d4af7103847df33eed6d3e1
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-09 15:27:58 +0530

    mul2

---
+++ 00_Basics/mul2.java
@@ -0,0 +1,10 @@
+public class mul2 {
+    public static void main(String[] args) {
+        int a=6;
+        int b=3;
+        int c=9;
+        int d=a*b*c;
+        System.out.println(d);
+    }
+    
+}
