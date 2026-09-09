e731a87d4c8aa7bc2fe65bea4e5f1542f1eb5444
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-09 15:06:52 +0530

    sum2

---
+++ 00_Basics/sum2.java
@@ -0,0 +1,9 @@
+public class sum2 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        int c=5;
+        int d=a+b+c;
+        System.out.println(d);
+    }
+}
