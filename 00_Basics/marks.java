11188ab9c2c3724a269595e6ec12b47c3f52cc45
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-09 17:01:58 +0530

    marks

---
+++ 00_Basics/marks.java
@@ -0,0 +1,17 @@
+public class marks {
+
+    public static void main(String[] args) {
+        char M2='A';
+        char beee='B';
+        char chemistry='A';
+        char english='A';
+        char ed='B';
+        System.out.println(M2);
+        System.out.println(beee);
+        System.out.println(chemistry);
+        System.out.println(english);
+        System.out.println(ed);
+        
+
+    }
+}
