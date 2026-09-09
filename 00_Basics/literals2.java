f59a8fb09b87abc974ab91b2810dd084e955c77d
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-09 21:25:22 +0530

    literals2

---
+++ 00_Basics/literals2.java
@@ -0,0 +1,30 @@
+public class literals2{
+    public static void main(String[] args){
+        byte age=34;
+        int age2=52;
+        short age3=72;
+        long age4=12345667;
+        char ch='A';
+        float f1=1.2f;
+        float f2=1.3f;
+        double d1=2.336;
+        double d2=1.369;
+        boolean a=true;
+        boolean b=false;
+        String str="Harry";
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(age4);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(str);
+        
+    }
+    
+}
