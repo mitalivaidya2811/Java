f38762ac006fa8e4678c72d374ebc6a901601d32
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-09 15:20:31 +0530

    sub2

---
+++ 00_Basics/sub2.java
@@ -0,0 +1,10 @@
+public class sub2 {
+    public static void main(String[] args) {
+        int a=6;
+        int b=5;
+        int c=3;
+        int d=a-b-c;
+        System.out.println(d);
+    }
+    
+}
