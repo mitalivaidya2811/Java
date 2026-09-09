f411fa28bc08d11cbd7f9e873a183a83c5f68a14
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-09 15:48:53 +0530

    sub3

---
+++ 00_Basics/sub3.java
@@ -0,0 +1,9 @@
+public class sub3 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int c=a-b;
+        System.out.println("the sub of these number is :"+c);
+    }
+    
+}
