73710d92a638f5fe1e3e09ee190c07cfa604e9d5
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-11 21:27:20 +0530

    num3

---
+++ 00_Basics/num2.java
@@ -0,0 +1,8 @@
+public class num2 {
+    public static void main(String[] args) {
+        int a =5;
+        int b =8*a;
+        System.out.println(b);
+    }
+    
+}
---
+++ 00_Basics/num3.java
@@ -0,0 +1,8 @@
+public class num3 {
+    public static void main(String[] args) {
+        int a =4;
+        a +=3;
+        System.out.println(a);
+    }
+    
+}
