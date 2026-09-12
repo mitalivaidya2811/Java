10e6a169e0731f9c0cd0a425aa35083acc69e376
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-12 11:21:18 +0530

    inde

---
+++ 00_Basics/inde.java
@@ -0,0 +1,12 @@
+public class inde {
+    public static void main(String[] args) {
+        int a =56;
+        System.out.println(a++);
+        System.out.println(--a);
+        int b = a++ -56;
+        System.out.println(b++);
+        System.out.println(--b);
+        System.out.println(b);
+    }
+    
+}
