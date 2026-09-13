c673ac2bd8438d0d84051c39f7808b6860362117
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-13 19:40:30 +0530

    logical6

--- 01_ConditionalStatement/logical6.java
+++ 01_ConditionalStatement/logical6.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class logical6 {
     public static void main(String[] args) {
+    System.out.print("Enter your age : ");
     Scanner sc = new Scanner(System.in);
     int age = sc.nextInt();
     if(age>56){
