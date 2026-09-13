26a36715a525cb112a04213b04009d231aeb63dc
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-13 15:09:57 +0530

    conditional1

---
+++ 01_ConditionalStatement/conditional1.java
@@ -0,0 +1,14 @@
+
+
+public class conditional1 {
+    public static void main(String[] args) {
+        int age = 6;
+        if(age>18){
+            System.out.println("yes boy you can drive");
+        }
+        else{
+            System.out.println("boy you cannot drive");
+        }
+    }
+    
+}
