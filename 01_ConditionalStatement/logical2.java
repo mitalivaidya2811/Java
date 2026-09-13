e200358e2dd341ec79448d5afa228ef8882e5880
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-13 16:25:42 +0530

    logical2

---
+++ 01_ConditionalStatement/logical2.java
@@ -0,0 +1,21 @@
+public class logical2 {
+    public static void main(String[] args) {
+        boolean a = false ;
+        boolean b = false;
+        if(a&&b){
+            System.out.println("t");
+        }
+        else{
+            System.out.println("f");
+        }
+        boolean c = true;
+        boolean d = true;
+        if(c&&d){
+            System.out.println("t");
+        }
+        else{
+            System.out.println("f");
+        }
+    }
+    
+}
