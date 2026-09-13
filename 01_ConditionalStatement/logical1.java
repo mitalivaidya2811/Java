4cd8fb14b3b7c0ecc27581de70c97fd4312ecdc3
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-13 16:18:51 +0530

    logical1

---
+++ 01_ConditionalStatement/logical1.java
@@ -0,0 +1,13 @@
+public class logical1 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b  = false;
+        if(a&&b){
+            System.out.println("t");
+        }
+        else{
+            System.out.println("f");
+        }
+    }
+    
+}
