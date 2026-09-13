8310f15285c81c714ffb9578b92e46615d4c2937
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-13 19:29:22 +0530

    logical5

---
+++ 01_ConditionalStatement/logical5.java
@@ -0,0 +1,18 @@
+public class logical5 {
+    public static void main(String[] args) {
+        int age = 56;
+        if(age>56){
+        System.out.println("you are experienced ");
+        }
+        else if(age>46){
+            System.out.println("you are semi experienced ");
+        }
+        else if(age>36){
+            System.out.println("you are semi-semi experienced ");
+        }
+        else{
+            System.out.println("you are not experienced ");
+        }
+    }
+    
+}
