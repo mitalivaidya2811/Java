5cf8dd80bbe9c505cc6b08f210633e000f4f5d25
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-13 16:38:23 +0530

    logical3

---
+++ 01_ConditionalStatement/logical3.java
@@ -0,0 +1,21 @@
+public class logical3 {
+    public static void main(String[] args) {
+        boolean  a = true;
+        boolean b = false;
+        boolean c = true;
+        System.out.print("for logical AND :");
+        if(a&&b&&c)
+            System.out.println("t");
+        else{
+            System.out.println("f");
+        }
+        System.out.print("for logical OR : ");
+        if(a||b||c)
+            System.out.println("t");
+        else{
+            System.out.println("f");
+        }
+        
+    }
+    
+}
