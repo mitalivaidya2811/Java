ab706e3a419882d37ce75b3219dbe0784cb3fc20
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-13 21:42:53 +0530

    casebased2

---
+++ 01_ConditionalStatement/casebased2.java
@@ -0,0 +1,20 @@
+public class casebased2 {
+public static void main(String[] args) {
+  char var = 'r';
+  switch (var) {
+    case 'r':
+        System.out.println("you are going to become an adult");
+   break;
+   case 'c':
+    System.out.println("you are going to join an job");
+    break;
+    case 'd':
+    System.out.println("you are going to get retiered ");
+    break;
+    default:
+    System.out.println("enjoy your life");
+        break;
+  }
+  System.out.println("thanks for using my code");
+}    
+}
---
+++ 01_ConditionalStatement/casebased3.java
@@ -0,0 +1,24 @@
+public class casebased3 {
+    public static void main(String[] args) {
+      String var = "Harry";
+  switch (var) {
+    case "Shubham":
+        System.out.println("you are going to become an adult");
+   break;
+   case "shivam":
+    System.out.println("you are going to join an job");
+    break;
+    case "Vishaka":
+    System.out.println("you are going to get retiered ");
+    break;
+    default:
+    System.out.println("enjoy your life");
+        break;
+  }
+  System.out.println("thanks for using my code");
+}    
+
+   
+    }
+    
+
