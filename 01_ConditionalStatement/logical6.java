509160a5b2cd7e16b83a53fd7ce7790e29b7a8ff
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-13 19:37:48 +0530

    logical6

---
+++ 01_ConditionalStatement/logical6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class logical6 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int age = sc.nextInt();
+    if(age>56){
+        System.out.println("you are experienced");
+
+    }
+    else if(age>46){
+        System.out.println("you are semi experienced");
+    }
+    else if(age>36){
+        System.out.println("you are semi-semi experienced");
+    }else{
+        System.out.println("you are not experienced");
+    }
+
+    sc.close();    
+    }
+    
+}
