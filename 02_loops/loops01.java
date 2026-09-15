dfb4b29a51bdfeed57b62c11da96feb912aade34
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-15 16:28:28 +0530

    loops01

---
+++ 02_loops/loops01.java
@@ -0,0 +1,17 @@
+
+
+public class loops01 {
+    public static void main(String[] args) {
+      int i = 1;
+        while(i<=9){
+           System.out.println(i);
+            i++;
+         // for(int i = 1; i<=9; i++){
+         //   System.out.println(i);
+        //  }
+          
+        }
+       System.out.println("Done"); 
+    }
+    
+}
