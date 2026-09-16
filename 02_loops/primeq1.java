92ffdff84615334eec45f12d50bbdcaf3909676a
Mitali Vaidya <mitalivaidya2811@gmail.com>
2026-09-16 15:51:33 +0530

    primeq1.java

--- 02_loops/primeq1.java
+++ 02_loops/primeq1.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class primeq1 {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
+        System.out.print("the number of test cases are : ");
         int t = sc.nextInt();
         for(int i = 1; i<=t ;i++){
+        System.out.print("the number are: ");
             int n =sc.nextInt();
             int count = 0;
             for( int div = 2; div*div<=n; div++){
@@ -12,7 +14,7 @@ public class primeq1 {
                     count++;
                     break;
                 }
-                
+
             }
             if(count==0){
                 System.out.println("prime");
